11b562997e1d6277dfb89d72e3f133dba8659893
AlyonaTranova <76480486+AlyonaTranova@users.noreply.github.com>
2021-06-16 20:00:48 +0300

    Update task_1.py

--- task_1.py
+++ task_1.py
@@ -4,7 +4,7 @@ from threading import Thread, BoundedSemaphore
 max_dining_philosophers = 2
 semaphore = BoundedSemaphore(max_dining_philosophers)
 
-def lunch_for_philosophers(i):
+def lunch_time(i):
     semaphore.acquire()
     try:
         print("Философ n. %i ест.\n" % i)
@@ -15,5 +15,5 @@ def lunch_for_philosophers(i):
         
 
 for i in range(5):
-    th = Thread(target=lunch_for_philosophers, args=(i + 1, ))
+    th = Thread(target=lunch_time, args=(i + 1, ))
     th.start()
